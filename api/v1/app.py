51e56bb377cc14df826590ac2edddcb4979315b4
zoraaga1 <fati.sousse12@gmail.com>
2024-03-31 17:15:45 +0000

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,10 @@ from api.v1.views import app_views
 from flask import Flask
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,8 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+
+
 @app_views.route('/status')
 def status():
     """Return status OK"""
