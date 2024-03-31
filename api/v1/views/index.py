13affa2906dc719b5e248378a11a4552f2d6fc2e
zoraaga1 <fati.sousse12@gmail.com>
2024-03-31 22:51:30 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,20 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Return status OK"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    """Return the number of each object by type."""
+    stats_data = {}
+    models = ["User", "State", "City", "Amenity", "Place", "Review"]
+    for model in models:
+        count = storage.count(model)
+        stats_data[model] = count
+    return jsonify(stats_data)
