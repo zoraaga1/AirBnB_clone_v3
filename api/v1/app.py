b8abc414c664eabca25c69d555fa1889ac09ef17
zoraaga1 <fati.sousse12@gmail.com>
2024-04-01 00:31:55 +0000

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask
 import os
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -18,6 +19,10 @@ def teardown_appcontext(exception):
     """Teardown app context"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
